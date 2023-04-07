b4b4092bf3e1799113551df54d87ad90c51bb21c
Sergey Kalinovsky <s.kalinovsky@godeltech.com>
2023-04-07 16:58:42 +0200

    Fixed ViewState instantiation issue

--- app/src/main/java/com/criticalgnome/cleanarchitecturedemo/ui/main/MainContract.kt
+++ app/src/main/java/com/criticalgnome/cleanarchitecturedemo/ui/main/MainContract.kt
@@ -4,8 +4,8 @@ import com.criticalgnome.domain.entity.PostModel
 
 interface MainContract {
     sealed interface ViewState {
-        data class PostsLoaded(val posts: List<PostModel>) : ViewState
-        data class ErrorHandled(val code: Int, val message: String?) : ViewState
-        data class ExceptionHandled(val throwable: Throwable) : ViewState
+        class PostsLoaded(val posts: List<PostModel>) : ViewState
+        class ErrorHandled(val code: Int, val message: String?) : ViewState
+        class ExceptionHandled(val throwable: Throwable) : ViewState
     }
 }
