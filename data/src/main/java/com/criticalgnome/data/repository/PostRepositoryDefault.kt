70ac4d3c8a54cb1a89ee707811242e102be694d2
Sergey Kalinovsky <lord.skiminok@gmail.com>
2023-04-07 23:41:24 +0200

    Reorganized ApiHandler

--- data/src/main/java/com/criticalgnome/data/repository/PostRepositoryDefault.kt
+++ data/src/main/java/com/criticalgnome/data/repository/PostRepositoryDefault.kt
@@ -1,9 +1,11 @@
 package com.criticalgnome.data.repository
 
+import com.criticalgnome.data.entity.CommentData
+import com.criticalgnome.data.entity.PostData
 import com.criticalgnome.data.mapper.CommentMapper
 import com.criticalgnome.data.mapper.PostMapper
+import com.criticalgnome.data.service.ApiHandler
 import com.criticalgnome.data.service.JsonPlaceholderService
-import com.criticalgnome.data.service.handleApi
 import com.criticalgnome.domain.entity.CommentModel
 import com.criticalgnome.domain.entity.PostModel
 import com.criticalgnome.domain.entity.Result
@@ -17,44 +19,44 @@ class PostRepositoryDefault @Inject constructor(
 ) : PostRepository {
 
     override suspend fun getPosts(): Result<List<PostModel>> {
-        return handleApi(
-            execute = { service.getPosts() },
-            mapping = { data -> data.map { postMapper.map(it) } }
-        )
+        return ApiHandler.Builder<List<PostData>, List<PostModel>>()
+            .setServiceExecutor { service.getPosts() }
+            .setMappingExecutor { data -> data.map { postMapper.map(it) } }
+            .handleApi()
     }
 
     override suspend fun getPost(id: Int): Result<PostModel> {
-        return handleApi(
-            execute = { service.getPost(id) },
-            mapping = { data -> postMapper.map(data) }
-        )
+        return ApiHandler.Builder<PostData, PostModel>()
+            .setServiceExecutor { service.getPost(id) }
+            .setMappingExecutor { data -> postMapper.map(data) }
+            .handleApi()
     }
 
     override suspend fun getPostComments(id: Int): Result<List<CommentModel>> {
-        return handleApi(
-            execute = { service.getPostComments(id) },
-            mapping = { data -> data.map { commentMapper.map(it) } }
-        )
+        return ApiHandler.Builder<List<CommentData>, List<CommentModel>>()
+            .setServiceExecutor { service.getPostComments(id) }
+            .setMappingExecutor { data -> data.map { commentMapper.map(it) } }
+            .handleApi()
     }
 
     override suspend fun postPosts(posts: List<PostModel>): Result<List<PostModel>> {
-        return handleApi(
-            execute = { service.postPosts(posts.map { postMapper.map(it) }) },
-            mapping = { data -> data.map { postMapper.map(it) } }
-        )
+        return ApiHandler.Builder<List<PostData>, List<PostModel>>()
+            .setServiceExecutor { service.postPosts(posts.map { postMapper.map(it) }) }
+            .setMappingExecutor { data -> data.map { postMapper.map(it) } }
+            .handleApi()
     }
 
     override suspend fun putPost(id: Int, post: PostModel): Result<PostModel> {
-        return handleApi(
-            execute = { service.putPost(id, postMapper.map(post)) },
-            mapping = { data -> postMapper.map(data) }
-        )
+        return ApiHandler.Builder<PostData, PostModel>()
+            .setServiceExecutor { service.putPost(id, postMapper.map(post)) }
+            .setMappingExecutor { data -> postMapper.map(data) }
+            .handleApi()
     }
 
     override suspend fun deletePost(id: Int): Result<Unit> {
-        return handleApi(
-            execute = { service.deletePost(id) },
-            mapping = { /* do nothing */ }
-        )
+        return ApiHandler.Builder<Unit, Unit>()
+            .setServiceExecutor { service.deletePost(id) }
+            .setMappingExecutor { /* do nothing */ }
+            .handleApi()
     }
 }
--- data/src/main/java/com/criticalgnome/data/service/ApiHandler.kt
+++ data/src/main/java/com/criticalgnome/data/service/ApiHandler.kt
@@ -4,16 +4,28 @@ import com.criticalgnome.domain.entity.Result
 import retrofit2.HttpException
 import retrofit2.Response
 
-suspend fun <T : Any, D: Any> handleApi(
-    execute: suspend () -> Response<T>,
-    mapping: (data: T) -> D
-): Result<D> {
-    return try {
-        val response = execute()
-        if (response.isSuccessful && response.body() != null) Result.Success(mapping(response.body() as T))
-        else Result.Error(response.code(), response.message())
-    } catch (e: Exception) {
-        if (e is HttpException) Result.Error(e.code(), e.message)
-        else Result.Exception(e)
+class ApiHandler<T : Any, D : Any> private constructor(
+    private val serviceExecute: suspend () -> Response<T>,
+    private val mappingExecute: (data: T) -> D
+) {
+    suspend fun handleApi(): Result<D> {
+        return try {
+            val response = serviceExecute()
+            if (response.isSuccessful && response.body() != null) Result.Success(mappingExecute(response.body() as T))
+            else Result.Error(response.code(), response.message())
+        } catch (e: Exception) {
+            if (e is HttpException) Result.Error(e.code(), e.message)
+            else Result.Exception(e)
+        }
+    }
+    class Builder<T : Any, D : Any> {
+        fun setServiceExecutor(execute: suspend () -> Response<T>): Builder2<T, D> {
+            return Builder2(execute)
+        }
+        class Builder2<T : Any, D : Any>(private val serviceExecute: suspend () -> Response<T>) {
+            fun setMappingExecutor(mappingExecute: (data: T) -> D): ApiHandler<T, D> {
+                return ApiHandler(serviceExecute, mappingExecute)
+            }
+        }
     }
 }
