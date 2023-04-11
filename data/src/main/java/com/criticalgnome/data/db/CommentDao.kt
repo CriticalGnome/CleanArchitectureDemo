79aaffe3a68074410ba49ec251049d6dd84d5553
Sergey Kalinovsky <s.kalinovsky@godeltech.com>
2023-04-11 09:53:32 +0200

    Updated dao interfaces

--- data/src/main/java/com/criticalgnome/data/db/CommentDao.kt
+++ data/src/main/java/com/criticalgnome/data/db/CommentDao.kt
@@ -1,11 +1,25 @@
 package com.criticalgnome.data.db
 
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 import com.criticalgnome.data.entity.CommentRoom
 
 @Dao
 interface CommentDao {
     @Query("SELECT * FROM comments")
     fun getComments(): List<CommentRoom>
+
+    @Query("SELECT * FROM comments WHERE id = :id")
+    suspend fun getComment(id: Int): CommentRoom
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertComments(vararg comments: CommentRoom)
+
+    @Update
+    suspend fun updateComments(vararg comments: CommentRoom)
+
+    @Query("DELETE * FROM comments WHERE id = :id")
+    suspend fun deleteById(id: Int)
+
+    @Delete
+    fun delete(comment: CommentRoom)
 }
--- data/src/main/java/com/criticalgnome/data/db/PostDao.kt
+++ data/src/main/java/com/criticalgnome/data/db/PostDao.kt
@@ -1,11 +1,25 @@
 package com.criticalgnome.data.db
 
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 import com.criticalgnome.data.entity.PostRoom
 
 @Dao
 interface PostDao {
     @Query("SELECT * FROM posts")
     suspend fun getPosts(): List<PostRoom>
+
+    @Query("SELECT * FROM posts WHERE id = :id")
+    suspend fun getPost(id: Int): PostRoom
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPosts(vararg posts: PostRoom)
+
+    @Update
+    suspend fun updatePosts(vararg posts: PostRoom)
+
+    @Query("DELETE * FROM posts WHERE id = :id")
+    suspend fun deleteById(id: Int)
+
+    @Delete
+    fun delete(post: PostRoom)
 }
