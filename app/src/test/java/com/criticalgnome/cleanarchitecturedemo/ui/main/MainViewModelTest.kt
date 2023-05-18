33c42f3350b14f39951149f33a5ca88c82adfe0a
Sergey Kalinovsky <s.kalinovsky@godeltech.com>
2023-05-18 12:14:39 +0200

    CleanArchitectureDemo-25 Refactor use cases to remove kotlin flow from result

--- app/src/test/java/com/criticalgnome/cleanarchitecturedemo/ui/main/MainViewModelTest.kt
+++ app/src/test/java/com/criticalgnome/cleanarchitecturedemo/ui/main/MainViewModelTest.kt
@@ -2,18 +2,22 @@ package com.criticalgnome.cleanarchitecturedemo.ui.main
 
 import app.cash.turbine.testIn
 import com.criticalgnome.cleanarchitecturedemo.TestDispatcherProvider
+import com.criticalgnome.cleanarchitecturedemo.ui.main.MainContract.ViewState.ErrorHandled
+import com.criticalgnome.cleanarchitecturedemo.ui.main.MainContract.ViewState.ExceptionHandled
 import com.criticalgnome.cleanarchitecturedemo.ui.main.MainContract.ViewState.PostsLoaded
-import com.criticalgnome.data.exception.TestNotWorkingProperlyException
 import com.criticalgnome.domain.entity.PostModel
+import com.criticalgnome.domain.entity.Result.Error
+import com.criticalgnome.domain.entity.Result.Exception
 import com.criticalgnome.domain.entity.Result.Success
 import com.criticalgnome.domain.usecase.GetPostsUseCase
 import io.mockk.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -24,6 +28,11 @@ class MainViewModelTest {
     private val testScope = CoroutineScope(dispatcherProvider.default)
     private lateinit var sut: MainViewModel
 
+    private val post = mockk<PostModel>()
+    private val errorCode = 404
+    private val errorMessage = "Page not found"
+    private val throwable = mockk<Throwable>()
+
     @BeforeEach
     fun setUp() {
         sut = MainViewModel(
@@ -32,16 +41,61 @@ class MainViewModelTest {
         )
     }
 
+    @AfterEach
+    fun tearDown() {
+        confirmVerified(getPostsUseCase)
+    }
+
     @Test
-    fun getPosts() {
-        val post = mockk<PostModel>()
-        coEvery { getPostsUseCase() } returns flowOf(Success(listOf(post)))
+    @DisplayName("GIVEN working use case WHEN getPosts invoked THEN return posts in success state")
+    fun getPostsSuccess() {
+        // given
+        coEvery { getPostsUseCase() } returns Success(listOf(post))
 
         runTest {
+            // when
             sut.getPosts()
             val state = sut.viewState.testIn(testScope).awaitItem()
-            if (state is PostsLoaded) assertEquals(listOf(post), state.posts)
-            else throw TestNotWorkingProperlyException()
+            // then
+            assertTrue(state is PostsLoaded)
+            assertEquals(listOf(post), (state as PostsLoaded).posts)
         }
+        coVerify { getPostsUseCase() }
+    }
+
+    @Test
+    @DisplayName("GIVEN error use case WHEN getPosts invoked THEN return error state")
+    fun getPostsError() {
+        // given
+        coEvery { getPostsUseCase() } returns Error(errorCode, errorMessage)
+
+        runTest {
+            // when
+            sut.getPosts()
+            val state = sut.viewState.testIn(testScope).awaitItem()
+            // then
+            assertTrue(state is ErrorHandled)
+            val error = state as ErrorHandled
+            assertEquals(errorCode, error.code)
+            assertEquals(errorMessage, error.message)
+        }
+        coVerify { getPostsUseCase() }
+    }
+
+    @Test
+    @DisplayName("GIVEN use case with exception WHEN getPosts invoked THEN return exception state")
+    fun getPostsException() {
+        // given
+        coEvery { getPostsUseCase() } returns Exception(throwable)
+
+        runTest {
+            // when
+            sut.getPosts()
+            val state = sut.viewState.testIn(testScope).awaitItem()
+            // then
+            assertTrue(state is ExceptionHandled)
+            assertEquals(throwable, (state as ExceptionHandled).throwable)
+        }
+        coVerify { getPostsUseCase() }
     }
 }
--- domain/src/test/java/com/criticalgnome/domain/usecase/GetPostsUseCaseDefaultTest.kt
+++ domain/src/test/java/com/criticalgnome/domain/usecase/GetPostsUseCaseDefaultTest.kt
@@ -1,20 +1,20 @@
 package com.criticalgnome.domain.usecase
 
-import app.cash.turbine.testIn
 import com.criticalgnome.domain.entity.PostModel
-import com.criticalgnome.domain.entity.Result
+import com.criticalgnome.domain.entity.Result.Error
+import com.criticalgnome.domain.entity.Result.Exception
+import com.criticalgnome.domain.entity.Result.Success
 import com.criticalgnome.domain.repository.PostRepository
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.confirmVerified
 import io.mockk.mockk
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -23,7 +23,10 @@ internal class GetPostsUseCaseDefaultTest {
     private val postRepository = mockk<PostRepository>()
     private lateinit var sut: GetPostsUseCase
 
-    private val testScope = CoroutineScope(UnconfinedTestDispatcher())
+    private val post = mockk<PostModel>()
+    private val errorCode = 404
+    private val errorMessage = "Page not found"
+    private val throwable = mockk<Throwable>()
 
     @BeforeEach
     fun setUp() {
@@ -36,12 +39,49 @@ internal class GetPostsUseCaseDefaultTest {
     }
 
     @Test
-    fun invoke() {
-        val result = mockk<Result<List<PostModel>>>()
-        coEvery { postRepository.getPosts() } returns result
+    @DisplayName("GIVEN working repository WHEN use case invoked THEN return success")
+    fun workingRepository() {
+        // given
+        coEvery { postRepository.getPosts() } returns Success(listOf(post))
+        runTest {
+            // when
+            val result = sut()
+            // then
+            assertTrue(result is Success)
+            assertEquals(listOf(post), (result as Success).data)
+        }
+        coVerify { postRepository.getPosts() }
+    }
 
-        runTest { assertEquals(result, sut().testIn(testScope).awaitItem()) }
+    @Test
+    @DisplayName("GIVEN error repository WHEN use case invoked THEN return error")
+    fun errorRepository() {
+        // given
+        coEvery { postRepository.getPosts() } returns Error(errorCode, errorMessage)
+        runTest {
+            // when
+            val result = sut()
+            // then
+            assertTrue(result is Error)
+            val error = result as Error
+            assertEquals(errorCode, error.code)
+            assertEquals(errorMessage, error.message)
+        }
+        coVerify { postRepository.getPosts() }
+    }
 
+    @Test
+    @DisplayName("GIVEN repository with exception WHEN use case invoked THEN return exception")
+    fun exceptionRepository() {
+        // given
+        coEvery { postRepository.getPosts() } returns Exception(throwable)
+        runTest {
+            // when
+            val result = sut()
+            // then
+            assertTrue(result is Exception)
+            assertEquals(throwable, (result as Exception).throwable)
+        }
         coVerify { postRepository.getPosts() }
     }
 }
